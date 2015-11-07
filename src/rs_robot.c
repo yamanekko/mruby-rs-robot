3e16ce17eb032efcdfb5cb7cb6080087d2640493
yuri <yamayuri@gmail.com>
2015-11-07 18:53:56 +0900

    add delay class method

--- src/rs_robot.c
+++ src/rs_robot.c
@@ -1,5 +1,7 @@
-#include "mruby.h"
-#include "mruby/variable.h"
+//#include "mruby.h"
+//#include "mruby/variable.h"
+
+#include "rs_robot.h"
 
 void mrb_mruby_rs_gyro_gem_init(mrb_state* mrb);
 void mrb_mruby_rs_motor_gem_init(mrb_state* mrb);
@@ -10,9 +12,27 @@ void mrb_mruby_rs_motor_gem_final(mrb_state* mrb);
 void mrb_mruby_rs_serial_gem_final(mrb_state* mrb);
 void mrb_mruby_rs_system_timer_gem_final(mrb_state* mrb);
 
+static mrb_value
+mrb_mruby_rs_robot_delay(mrb_state *mrb, mrb_value self)
+{	//int divisor
+    unsigned int ra2;
+	uint32_t pwm_control;
+
+    mrb_int wait;
+
+    mrb_get_args(mrb, "i", &wait);	//TODO 引数の値は使用してない
+//    wait &= 4095;
+//    wait = wait * 1000;
+    for(ra2=0;ra2<wait;ra2++) dummy(ra2);
+    return self;
+}
+
 void
 mrb_mruby_rs_robot_gem_init(mrb_state* mrb) {
-    // call gem initializer
+	struct RClass * robot_class = mrb_define_class(mrb, "RSRobot", mrb->object_class);
+	mrb_define_class_method(mrb, robot_class, "delay", mrb_mruby_rs_robot_delay, MRB_ARGS_REQ(1));
+
+	// call gem initializer
     mrb_mruby_rs_gyro_gem_init(mrb);
     mrb_mruby_rs_motor_gem_init(mrb);
     mrb_mruby_rs_serial_gem_init(mrb);
